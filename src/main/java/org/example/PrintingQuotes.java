af96517b839ca63be1ac7f315aa8814a57f6c321
Sebastian <SebastianLSteele@knights.ucf.edu>
2021-06-05 23:43:09 -0400

    committing gitignore and exercise java file

--- src/main/java/org/example/PrintingQuotes.java
+++ src/main/java/org/example/PrintingQuotes.java
@@ -1,2 +1,26 @@
-package org.example;public class PrintingQuotes {
+package org.example;
+
+
+import java.util.Scanner;
+
+public class PrintingQuotes {
+// Quotation marks are often used to denote the start and end of a string.
+// But sometimes we need to print out the quotation marks themselves by using escape characters.
+//
+//Create a program that prompts for a quote and an author. Display the quotation and author as shown in the example output.
+    public static void main(String[] args) {
+
+        Scanner getQuote = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = getQuote.nextLine();
+
+
+        Scanner getAuthor = new Scanner(System.in);
+        System.out.print("Who said it ");
+        String author = getAuthor.nextLine();
+
+         System.out.print(author +  " says, \"" + quote+ "\"");
+
+    }
+
 }
